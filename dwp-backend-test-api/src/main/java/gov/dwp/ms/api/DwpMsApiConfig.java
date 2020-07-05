0623cf71111d6c7a30f29a7c024fc4d562b81324
neillbroderick <neill.broderick@yahoo.co.uk>
2020-07-05 13:34:38 +0100

    Fixing indentation

--- dwp-backend-test-api/src/main/java/gov/dwp/ms/api/DwpMsApiConfig.java
+++ dwp-backend-test-api/src/main/java/gov/dwp/ms/api/DwpMsApiConfig.java
@@ -16,14 +16,14 @@ public class DwpMsApiConfig {
     @Bean
     public Docket dwpApi() { // Bean to create Swagger documents from api calls. Can be accessed using https://localhost:8443/swagger-ui.html.
     	return new Docket(DocumentationType.SWAGGER_2)  
-				.apiInfo(new ApiInfoBuilder()
-				        .title("DWP Users API Microservice") // Swagger UI title & description below.
-				        .description("Our API (Application Programming Interface) enables you to integrate with us and build applications that leverage our functionality and data.")
-				        .version("1.0")
-				        .build())
-				.select()                                  
-				.apis(RequestHandlerSelectors.any())              
-				.paths(PathSelectors.any()) // Selecting all paths, but can limit to specific apis using regex if required.                         
-				.build();
+                .apiInfo(new ApiInfoBuilder()
+                         .title("DWP Users API Microservice") // Swagger UI title & description below.
+                         .description("Our API (Application Programming Interface) enables you to integrate with us and build applications that leverage our functionality and data.")
+                         .version("1.0")
+                         .build())
+                .select()                                  
+                .apis(RequestHandlerSelectors.any())              
+                .paths(PathSelectors.any()) // Selecting all paths, but can limit to specific apis using regex if required.                         
+                .build();
     }   
 }
