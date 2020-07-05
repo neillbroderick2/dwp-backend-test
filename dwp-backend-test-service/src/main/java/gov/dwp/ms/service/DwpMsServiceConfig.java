23356ba79daa362a1fd3505e105d5e35a4b7f9c7
neillbroderick <neill.broderick@yahoo.co.uk>
2020-07-05 13:48:05 +0100

    Fixing indentation

--- dwp-backend-test-service/src/main/java/gov/dwp/ms/service/DwpMsServiceConfig.java
+++ dwp-backend-test-service/src/main/java/gov/dwp/ms/service/DwpMsServiceConfig.java
@@ -26,13 +26,13 @@ public class DwpMsServiceConfig {
 	@Value("${thread.queue.capacity}")
     private int queueCapacity;
 	
-	@Bean
+    @Bean
     public ThreadPoolTaskExecutor dwpTaskExec() { // Bean to create a thread pool that is used by the future methods in the service.
-		// There are 3 futures that may need to run concurrently in this service.
-		// Thread pool is setup to use core pool size of 3 times the Tomcat 200 connection default limit.
-		// This will enable the program to always have 3 threads available for each potential Tomcat connection.
-		// Max pool size is set also to 600 to create a fixed size thread pool based on available connections.
-		// Queue capacity is set to 300, which means we could potentially queue another 100 requests whilst waiting for busy threads to become available (Tomcat limit may need increasing). 
+        // There are 3 futures that may need to run concurrently in this service.
+        // Thread pool is setup to use core pool size of 3 times the Tomcat 200 connection default limit.
+        // This will enable the program to always have 3 threads available for each potential Tomcat connection.
+        // Max pool size is set also to 600 to create a fixed size thread pool based on available connections.
+        // Queue capacity is set to 300, which means we could potentially queue another 100 requests whilst waiting for busy threads to become available (Tomcat limit may need increasing). 
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
     	executor.setCorePoolSize(corePoolSize);
     	executor.setMaxPoolSize(maxPoolSize);
@@ -43,7 +43,7 @@ public class DwpMsServiceConfig {
         return executor;
     }
 	
-	@Bean
+    @Bean
     public RestTemplate restTemplate() throws Exception { // Bean to setup a basic rest template to make api calls.
         return new RestTemplate();
     }
