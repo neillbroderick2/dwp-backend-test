88b3e093cc37230472134571e54058528e75d859
neillbroderick <neill.broderick@yahoo.co.uk>
2020-07-05 13:16:19 +0100

    Updating indentation

--- dwp-backend-test-api/src/main/java/gov/dwp/ms/api/DwpMsApiHelper.java
+++ dwp-backend-test-api/src/main/java/gov/dwp/ms/api/DwpMsApiHelper.java
@@ -25,31 +25,31 @@ public class DwpMsApiHelper {
 		Set<APIUsersResponse> resultSet = new HashSet<APIUsersResponse>(); // Create a result set to contain the valid results.
 		
 		if (radiusUsers.isEmpty() || (location.getLat() == null) || (location.getLon() == null)) {
-        	throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR); // There should always be location / radiusUsers so throw an error to prevent null pointer.
-        }
+			throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR); // There should always be location / radiusUsers so throw an error to prevent null pointer.
+		}
 		
 		if (!searchUsers.isEmpty()) {
-        	resultSet.addAll(searchUsers); // If there are some users from the city search result, add them to the result set as the city matched.
-        }
+			resultSet.addAll(searchUsers); // If there are some users from the city search result, add them to the result set as the city matched.
+		}
 		
-        for (APIUsersResponse u : radiusUsers) { // Loop through radius api call users.
-        	GeodesicData result = Geodesic.WGS84.Inverse(Double.parseDouble(location.getLat()), Double.parseDouble(location.getLon()), u.getLatitude(), u.getLongitude()); // Get the geodesic data result, by passing the source lat/lon and users lat/lon.
-        	
-            double distanceInMiles = result.s12 / 1609.34; // Result is in meters so divide to convert it into miles.
-            	
-            if (distanceInMiles <= radius) { // If the distance is less than radius, add the user.
-            	resultSet.add(u); // Sets can only contain unique list values so this will prevent duplicates from the two api calls appearing in the results.
-            }
-        }
-
-        if (resultSet.isEmpty()) {
-        	throw new ResponseStatusException(HttpStatus.NO_CONTENT); // If there are no results to display, throw a 204 NO CONTENT message back to the requester.
-        }
-        
-        List<APIUsersResponse> response = new ArrayList<APIUsersResponse>(resultSet); 
-        Collections.sort(response, new DwpMsUserSort()); // Sort the results by id before returning, so they are all nicely in the correct order.
-        
-        return response;
+		for (APIUsersResponse u : radiusUsers) { // Loop through radius api call users.
+			GeodesicData result = Geodesic.WGS84.Inverse(Double.parseDouble(location.getLat()), Double.parseDouble(location.getLon()), u.getLatitude(), u.getLongitude()); // Get the geodesic data result, by passing the source lat/lon and users lat/lon.
+			
+		    double distanceInMiles = result.s12 / 1609.34; // Result is in meters so divide to convert it into miles.
+		    	
+		    if (distanceInMiles <= radius) { // If the distance is less than radius, add the user.
+		    	resultSet.add(u); // Sets can only contain unique list values so this will prevent duplicates from the two api calls appearing in the results.
+		    }
+		}
+		
+		if (resultSet.isEmpty()) {
+			throw new ResponseStatusException(HttpStatus.NO_CONTENT); // If there are no results to display, throw a 204 NO CONTENT message back to the requester.
+		}
+		
+		List<APIUsersResponse> response = new ArrayList<APIUsersResponse>(resultSet); 
+		Collections.sort(response, new DwpMsUserSort()); // Sort the results by id before returning, so they are all nicely in the correct order.
+		
+		return response;
 	}
 	
 	public String checkRequestId(String xCorrelationId) { // Checks if there is a request id passed by the requester & generates one if not.
